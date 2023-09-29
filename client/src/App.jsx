67880ed403468dc572421bb538f561a1a2ac13f1
Klaudia Paniuk <kpaniuk@gmail.com>
2023-09-29 22:56:23 +0200

    DashboardPage, Navigation, Currency, Balance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/homePage/HomePage';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage/HomePage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import CurrencyPage from "./pages/CurrencyPage/CurrencyPage";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path='/' element={<HomePage />} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/home" element={<DashboardPage />} />
+				<Route path="/currency" element={<CurrencyPage />} />
 			</Routes>
 		</>
 	);
