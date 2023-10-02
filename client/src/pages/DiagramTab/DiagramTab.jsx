af567b6557f8f4fa48e215d3006e5fb52c3ff1a8
SzymonSliwa <creedo@tlen.pl>
2023-10-02 19:17:26 +0200

    fix diagramTab i buttonAdd fixed
    added Header and Navigation

--- client/src/pages/DiagramTab/DiagramTab.jsx
+++ client/src/pages/DiagramTab/DiagramTab.jsx
@@ -1,22 +1,26 @@
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { selectTransactionsData } from '../../redux/finance/financeSelectors';
-import Chart from '../../components/Chart/Chart';
-import TableChart from '../../components/Chart/Table/TableChart';
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { selectTransactionsData } from "../../redux/finance/financeSelectors";
+import Chart from "../../components/Chart/Chart";
+import TableChart from "../../components/Chart/Table/TableChart";
+import Navigation from "../../components/Navigation/Navigation";
 
-import css from './DiagramTab.module.css';
+import css from "./DiagramTab.module.css";
+import Header from "../../components/Header/Header";
 
 const DiagramTab = () => {
-	const dataToRender = useSelector(selectTransactionsData);
+  const dataToRender = useSelector(selectTransactionsData);
 
-	return (
-		<div className={css.container}>
-			<p className={css.diagramTitle}>Statistic</p>
-			<div className={css.diagramWrap}>
-				<Chart dataToRender={dataToRender} />
-				<TableChart dataToRender={dataToRender} />
-			</div>
-		</div>
-	);
+  return (
+    <div className={css.container}>
+      <Header />
+      <Navigation />
+      <p className={css.diagramTitle}>Statistic</p>
+      <div className={css.diagramWrap}>
+        <Chart dataToRender={dataToRender} />
+        <TableChart dataToRender={dataToRender} />
+      </div>
+    </div>
+  );
 };
 
 export default DiagramTab;
