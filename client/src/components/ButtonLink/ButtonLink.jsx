c3707963a5d4c285e6f39c8098a12b3c4d1ee4d8
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-10-03 12:04:33 +0200

    stlowanie btns

--- client/src/components/ButtonLink/ButtonLink.jsx
+++ client/src/components/ButtonLink/ButtonLink.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import PropTypes from "prop-types";
+//
+import PropTypes from 'prop-types';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import clsx from "clsx";
-import css from "../ButtonLink/ButtonLink.module.css";
+import clsx from 'clsx';
+import css from '../ButtonLink/ButtonLink.module.css';
 
 const ButtonLink = ({ navigate, text }) => {
-  return (
-    <Link className={clsx(css.btnLink)} to={navigate}>
-      {text}
-    </Link>
-  );
+	return (
+		<Link className={clsx(css.btnLink)} to={navigate}>
+			{text}
+		</Link>
+	);
 };
 
 ButtonLink.propTypes = {
-  navigate: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+	navigate: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
 };
 
 export default ButtonLink;
